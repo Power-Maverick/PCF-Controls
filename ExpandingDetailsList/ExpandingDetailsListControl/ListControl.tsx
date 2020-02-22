1de03bd2ea1c42137d274b4163c3cf7c2fd3963b
Danish Naglekar <dnaglekar@akaes.com>
2020-02-22 17:33:02 -0500

    Initi Checkin

---
+++ ExpandingDetailsList/ExpandingDetailsListControl/ListControl.tsx
@@ -0,0 +1,288 @@
+import * as React from 'react';
+import { Link } from 'office-ui-fabric-react/lib/Link';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import { HoverCard, IPlainCardProps, HoverCardType } from 'office-ui-fabric-react/lib/HoverCard';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { ScrollablePane, ScrollbarVisibility } from 'office-ui-fabric-react/lib/ScrollablePane';
+import { DetailsList, IColumn, DetailsListLayoutMode, ConstrainMode, IDetailsFooterProps, Selection, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
+import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
+import { IRenderFunction } from 'office-ui-fabric-react/lib/Utilities';
+import { Sticky, StickyPositionType } from 'office-ui-fabric-react/lib/Sticky';
+
+//#region Style Constants
+
+const classNames = mergeStyleSets({
+    plainCard: {
+        width: 400,
+        //height: 600,
+        padding: '20px 10px',
+        height: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        whiteSpace: 'pre-wrap'
+    },
+    item: {
+        selectors: {
+            '&:hover': {
+                cursor: 'pointer'
+            }
+        }
+    },
+    listFooter: {
+        display: 'flex',
+        padding: '1px'
+    },
+    listFooterLabel: {
+        color: '#6E6F77',
+        fontSize: '0.857143rem'
+    }
+});
+
+//#endregion
+
+//#region Interfaces
+export interface IListControlProps {
+    data: IListData[];
+    columns: IListColumn[];
+    expandingColumns: string;
+    triggerNavigate?: (id: string) => void;
+    triggerPaging?: (pageCommand: string) => void;
+    triggerSelection?: (selectedKeys: any[]) => void;
+}
+
+export interface IListData {
+    attribute: string;
+    value: string;
+}
+
+export interface IListColumn extends IColumn {
+    dataType?: string,
+    isPrimary?: boolean
+}
+
+export interface IListControlState extends React.ComponentState {
+}
+//#endregion 
+
+export class ListControl extends React.Component<IListControlProps, IListControlState> {
+
+    //#region Global Variables
+    private _selection: Selection;
+    private _totalWidth: number;
+    private _expandingColumns: string[];
+    //#endregion
+
+    constructor(props: IListControlProps) {
+        super(props);
+
+        this._totalWidth = this._totalColumnWidth(props.columns);
+        this._expandingColumns = this._parseExpandingColumns(props.expandingColumns);
+
+        this.state = {
+            _items: props.data,
+            _columns: this._buildColumns(props.columns),
+            _triggerNavigate: props.triggerNavigate,
+            _triggerSelection: props.triggerSelection,
+            _selectionDetails: 'No items selected'
+        };
+
+        this._selection = new Selection({
+            onSelectionChanged: () => {
+                this.setState({
+                    _selectionDetails: this._setSelectionDetails(),
+                });
+            }
+        });
+    }
+
+    public componentWillReceiveProps(newProps: IListControlProps): void {
+        this.setState({
+            _items: newProps.data,
+            _columns: this._buildColumns(newProps.columns)
+        });
+        this._totalWidth = this._totalColumnWidth(newProps.columns);
+    }
+
+    //#region Private functions
+    private _onRenderShowPlainHoverCard = (item?: any, index?: number, column?: IColumn): JSX.Element | React.ReactText => {
+        const plainCardProps: IPlainCardProps = {
+            onRenderPlainCard: this._onRenderPlainCard,
+            renderData: column ? item[column.key as keyof IListData] : ""
+        };
+
+        if (column) {
+            return (
+                <HoverCard plainCardProps={plainCardProps} instantOpenOnClick={true} type={HoverCardType.plain}>
+                    <div className={classNames.item}>{item[column.key as keyof IListData]}</div>
+                </HoverCard>
+            );
+        }
+
+        // Return empty if column is NULL
+        return "";
+    }
+
+    private _onRenderPlainCard = (item: string): JSX.Element => {
+        return (
+            <div className={classNames.plainCard}>
+                {item}
+            </div>
+        );
+    };
+
+    private _onRenderDetailsFooter = (props: IDetailsFooterProps | undefined, defaultRender?: IRenderFunction<IDetailsFooterProps>): JSX.Element => {
+        return (
+            <Sticky stickyPosition={StickyPositionType.Footer} isScrollSynced={true}>
+                <div className={classNames.listFooter}>
+                    <Label className={classNames.listFooterLabel}>{this.state._items.length.toString()} ({this.state._selectionDetails})</Label>
+                </div>
+            </Sticky>
+        );
+    }
+
+    private _onColumnClick = (ev?: React.MouseEvent<HTMLElement>, column?: IColumn): void => {
+        let updatedColumns: IColumn[] = this.state._columns;
+        let sortedItems: IListData[] = this.state._items;
+        let isSortedDescending: boolean | undefined = column?.isSortedDescending;
+    
+        if (column?.isSorted) {
+          isSortedDescending = !isSortedDescending;
+        }
+    
+        sortedItems = this._sort(sortedItems, column?.fieldName!, isSortedDescending);
+    
+        this.setState({
+            _items: sortedItems,
+            _columns:
+                updatedColumns.map(col => {
+                    col.isSorted = col.key === column?.key;
+                    if (col.isSorted) {
+                        col.isSortedDescending = isSortedDescending;
+                    }
+                    return col;
+                })
+        });
+    }
+
+    private _setSelectionDetails(): string {
+        let selectedKeys = [];
+        let selections = this._selection.getSelection();
+        for (let selection of selections) {
+            selectedKeys.push(selection.key as string);
+        }
+
+        this.state._triggerSelection(selectedKeys);
+
+        switch (selectedKeys.length) {
+            case 0:
+                return 'No items selected';
+            default:
+                return `${selectedKeys.length} items selected`;
+        }
+    }
+
+    private _sort = <T, >(items: T[], columnKey: string, isSortedDescending?: boolean): T[] =>  {
+        let key = columnKey as keyof T;
+        return items.slice(0).sort((a: T, b: T) => ((isSortedDescending ? a[key] < b[key] : a[key] > b[key]) ? 1 : -1));
+    }
+
+    private _buildColumns(listData: IListColumn[]): IColumn[] {
+        let iColumns: IColumn[] = [];
+
+        for (var column of listData){
+            let iColumn: IColumn = {
+                key: column.key,
+                name: column.name,
+                fieldName: column.fieldName,
+                currentWidth: column.currentWidth,
+                minWidth: column.minWidth,                
+                maxWidth: column.maxWidth,
+                isResizable: column.isResizable,
+                sortAscendingAriaLabel: column.sortAscendingAriaLabel,
+                sortDescendingAriaLabel: column.sortDescendingAriaLabel,
+                className: column.className,
+                headerClassName: column.headerClassName,
+                data: column.data,
+                isSorted: column.isSorted,
+                isSortedDescending: column.isSortedDescending
+            }
+            
+            //create links for primary field and entity reference.            
+            if (column.dataType && (column.dataType === "Lookup" || column.isPrimary))
+            {
+                iColumn.onRender = (item: any, index: number | undefined, column: IColumn | undefined) => (
+                    <Link key={item.key} onClick={() => this.state._triggerNavigate(item.key)}>{item[column?.fieldName!]}</Link>
+                );
+            }
+            else if(column.dataType === 'Email'){
+                iColumn.onRender = (item: any, index: number | undefined, column: IColumn | undefined)=> (                                    
+                    <Link href={'mailto:${item[column?.fieldName!]}'} >{item[column?.fieldName!]}</Link>  
+                );
+            }
+            else if(column.dataType === 'Phone'){
+                iColumn.onRender = (item: any, index: number | undefined, column: IColumn | undefined) => (                                    
+                    <Link href={'skype:${item[column?.fieldName!]}?call'} >{item[column!.fieldName!]}</Link>                    
+                );
+            }
+            else if(this._expandingColumns && this._expandingColumns.includes(column.key)){
+                iColumn.onRender = this._onRenderShowPlainHoverCard;
+            }
+
+            iColumns.push(iColumn);
+        }
+
+        return iColumns;
+    }
+
+    private _totalColumnWidth(listData: IListColumn[]): number {
+        let totalColumnWidth: number;
+
+        totalColumnWidth = listData
+                            .map(v => v.maxWidth!)
+                            .reduce((sum, current) => sum + current);
+
+        // Add extra buffer
+        return totalColumnWidth + 100;
+    }
+
+    private _parseExpandingColumns(commaSeparatedString: string): string[]
+    {
+        return commaSeparatedString.split(",");
+    }
+    //#endregion
+
+    //#region Main Render Function
+    public render() {
+        const styles = {
+            divWidth: {
+                width: this._totalWidth,
+            }
+        };
+
+        return (
+            <Fabric>
+                <ScrollablePane scrollbarVisibility={ScrollbarVisibility.auto}>
+                    <div style = {styles.divWidth}>
+                        <DetailsList
+                            setKey="parentcustomerid"
+                            items={this.state._items}
+                            columns={this.state._columns}
+                            onColumnHeaderClick={this._onColumnClick}
+                            layoutMode={DetailsListLayoutMode.justified}
+                            constrainMode={ConstrainMode.unconstrained}
+                            selection={this._selection}
+                            selectionPreservedOnEmptyClick={true}
+                            selectionMode={SelectionMode.multiple}
+                            onRenderDetailsFooter={this._onRenderDetailsFooter} 
+                            ariaLabelForSelectionColumn="Toggle selection"
+                            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+                            checkButtonAriaLabel="Row checkbox"
+                        />
+                    </div>
+                </ScrollablePane>
+            </Fabric>
+        );
+    }
+    //#endregion
+}
---
+++ ExpandingDetailsList/ExpandingDetailsListControl/index.ts
@@ -0,0 +1,207 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ListControl, IListControlProps, IListControlState, IListData, IListColumn } from './ListControl';
+import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+
+export class ExpandingDetailsListControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	// React properties
+	private _props: IListControlProps = {
+		data: [],
+		columns: [],
+		expandingColumns: ""
+	};
+
+	/**
+	 * Global Variables
+	 */
+	private theContainer: HTMLDivElement;
+	private theNotifyOutputChanged: () => void;
+	private theContext: ComponentFramework.Context<IInputs>;
+
+	/**
+	 * Empty constructor.
+	 */
+	constructor() {
+
+	}
+
+	/**
+	 * HTML Attributes
+	 */
+	private divDetailListWrapper: HTMLDivElement;
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
+		this.theNotifyOutputChanged = notifyOutputChanged;
+		this.theContainer = container;
+		this.theContext = context;
+
+		this._props.triggerNavigate = this.navigateToRecord.bind(this);
+		this._props.triggerPaging = this.navigateToPage.bind(this);
+		this._props.triggerSelection = this.recordSelection.bind(this);
+		
+		context.mode.trackContainerResize(true);
+		this.theContainer.style.position = 'relative';
+
+		this.divDetailListWrapper = document.createElement("div");
+		this.divDetailListWrapper.setAttribute("id", "detailList");
+		this.divDetailListWrapper.setAttribute("data-is-scrollable", "true");
+		let rowspan = (this.theContext.mode as any).rowSpan;
+		let height = (rowspan * 2) + 4 /*Header*/ + 4 /*Footer*/;
+		if (rowspan) {
+			this.divDetailListWrapper.style.height = `${height}em`;
+		}
+
+		this.theContainer.appendChild(this.divDetailListWrapper);
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
+		const dataSet = context.parameters.listDataSet;
+
+		let datasetColumns: IListColumn[] = this._columns(dataSet, context.mode.allocatedWidth === -1 ? 150 : context.mode.allocatedWidth);
+		let dataItems: IListData[] = this._items(dataSet, datasetColumns);
+		this._props.data = dataItems;
+		this._props.columns = datasetColumns;
+		this._props.expandingColumns = context.parameters.expandingColumns.raw || "";
+
+		ReactDOM.render(
+			React.createElement(ListControl, this._props),
+			this.divDetailListWrapper
+		);
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs {
+		return {};
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void {
+		ReactDOM.unmountComponentAtNode(this.divDetailListWrapper);
+	}
+
+
+	/********** PRIVATE PROPERTIES & FUNCTIONS **********/
+
+	// Get the columns from the dataset
+	private _columns = (ds: DataSet, totalWidth: number): IListColumn[] => {
+		let dataSet = ds;
+		let iColumns: IListColumn[] = [];
+
+		for (var column of dataSet.columns) {
+			let iColumn: IListColumn = {
+				key: column.name,
+				name: column.displayName,
+				fieldName: column.alias,
+				currentWidth: column.visualSizeFactor,
+				data: { isPrimary: column.isPrimary },
+				minWidth: 5,
+				maxWidth: column.visualSizeFactor,
+				isResizable: true,
+				sortAscendingAriaLabel: 'A to Z',
+				sortDescendingAriaLabel: 'Z to A',
+				className: 'detailList-cell',
+				headerClassName: 'detailList-gridLabels',
+				isPrimary: column.isPrimary
+			}
+
+			//create links for primary field and entity reference.            
+			if (column.dataType.startsWith('Lookup.') || column.isPrimary) {
+				iColumn.dataType = "Lookup";
+			}
+			else if (column.dataType === 'SingleLine.Email') {
+				iColumn.dataType = "Email";
+			}
+			else if (column.dataType === 'SingleLine.Phone') {
+				iColumn.dataType = "Phone";
+			}
+
+			let isSorted = dataSet?.sorting?.findIndex(s => s.name === column.name) !== -1 || false;
+			iColumn.isSorted = isSorted;
+			if (isSorted) {
+				iColumn.isSortedDescending = dataSet?.sorting?.find(s => s.name === column.name)?.sortDirection === 1 || false;
+			}
+
+			iColumns.push(iColumn);
+		}
+		return iColumns;
+	}
+
+	// Get the items from the dataset
+	private _items = (ds: DataSet, _columns: IListColumn[]) => {
+		let dataSet = ds;
+
+		var resultSet = dataSet.sortedRecordIds.map(function (key) {
+			var record = dataSet.records[key];
+			var newRecord: any = {
+				key: record.getRecordId()
+			};
+
+			for (var column of _columns) {
+				newRecord[column.key] = record.getFormattedValue(column.key);
+				/*let checkER: any = record.getValue(column.key);
+				if (typeof checkER?.etn === 'string') {
+					var ref = record.getValue(column.key) as ComponentFramework.EntityReference;
+					newRecord[column.key + '_ref'] = ref;
+				}
+				else if (column.data.isPrimary) {
+					newRecord[column.key + '_ref'] = record.getNamedReference();
+				}*/
+			}
+
+			return newRecord;
+		});
+
+		return resultSet;
+	}
+
+	private navigateToPage(pageCommand: string): void {
+		switch (pageCommand) {
+			case 'next':
+				if (this.theContext.parameters.listDataSet.paging.hasNextPage) {
+					this.theContext.parameters.listDataSet.paging.loadNextPage();
+				}
+				break;
+			case 'previous':
+				if (this.theContext.parameters.listDataSet.paging.hasPreviousPage) {
+					this.theContext.parameters.listDataSet.paging.loadPreviousPage();
+				}
+				break;
+		}
+	}
+
+	private navigateToRecord(id: string): void {
+		let record: any = this.theContext.parameters.listDataSet.records[id].getNamedReference();
+		console.log(record);
+		this.theContext.navigation.openForm({
+			entityName: record.entityName,
+			entityId: record.id
+		});
+	}
+
+	private recordSelection(selectedKeys: any[]): void {
+		this.theContext.parameters.listDataSet.setSelectedRecordIds(selectedKeys);
+	}
+
+}
